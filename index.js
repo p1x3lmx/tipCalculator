3f8ab15ecdf81dc2561c6c206e0a605d28e80ab3
Francisco Medina <medina.juanfrancisco@gmail.com>
2019-06-16 22:50:40 -0500

    JS Tip Calculator

---
+++ index.js
@@ -0,0 +1,14 @@
+// Get value from text input and assign to variable total
+function tipAmount(clicked_byId) {
+  let totalAmount = document.getElementById("totalAmount").value;
+  if (isNaN(totalAmount)){ // If statement to validate the number typed
+    alert("Be Sure that you have typed a valid amount");
+  } else {
+    let clickID = clicked_byId;
+    console.log(clickID);
+    let granTotal = ((clickID/100)+1) * totalAmount;
+    console.log(totalAmount);
+    console.log(granTotal);
+    document.getElementById("gt").value = "$" + granTotal.toFixed(2);
+  }
+}
