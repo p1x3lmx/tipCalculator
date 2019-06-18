276115509a67d98ea0eab6d76ed4f9313a12d176
Francisco Medina <medina.juanfrancisco@gmail.com>
2019-06-17 22:02:21 -0500

    Format change in CSS font and style

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ function tipAmount(clicked_byId) {
     alert("Be Sure that you have typed a valid amount");
   } else {
     let clickID = clicked_byId;
-    console.log(clickID);
     let granTotal = (1+(clickID/100)) * totalAmount;
-    console.log(totalAmount);
-    console.log(granTotal);
     document.getElementById("gt").value = "$" + granTotal.toFixed(2);
   }
 }
